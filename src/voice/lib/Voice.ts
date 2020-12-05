e197dfa673b13de9b34e04913d3ba7aa371a866d
Cqdet <cadetmercury615@gmail.com>
2020-12-05 12:48:39 -0500

    chore: Start native VC support

---
+++ src/voice/lib/Voice.ts
@@ -0,0 +1,13 @@
+export default class Voice {
+	private callNativeSync(op: string, ...args: any[]) {
+		/**
+		 * Deno.openPlugin(...)
+		 */
+	}
+
+	private async callNative(op: string, ...args: any[]) {
+		/**
+		 * Deno.openPlugin(...)
+		 */
+	}
+}
---
+++ src/voice/src/lib.rs
@@ -0,0 +1,17 @@
+// FROM DENO: https://github.com/denoland/deno/blob/master/test_plugin/src/lib.rs
+
+use deno_core::plugin_api::Interface;
+use deno_core::plugin_api::Op;
+use deno_core::plugin_api::ZeroCopyBuf;
+use futures::future::FutureExt;
+
+#[no_mangle]
+pub fn deno_plugin_init(interface: &mut dyn Interface) {
+    interface.register_op("testSync", op_test_sync);
+}
+
+fn op_test_sync(_interface: &mut dyn Interface, zero_copy: &mut [ZeroCopyBuf]) -> Op {
+    let result = b"Hello World";
+    let result_box: Box<[u8]> = Box::new(*result);
+    Op::Sync(result_box)
+}
